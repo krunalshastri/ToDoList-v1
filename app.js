740a115fc3e8335c851725082e1359a82e40f685
Krunal Shastri <anonymousheeo007@gmail.com>
2021-02-22 22:04:40 +0530

    Added header, footer and date.js(module)

--- app.js
+++ app.js
@@ -2,31 +2,49 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-var vaars = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-var items = [];
+const date = require(__dirname + "/date.js");
+// console.log(date());
+
+let items = [];
+let workItems = [];
 const app = express();
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.get("/",function(req,res)
 {
-  var today_date = new Date();
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-  var today_day = today_date.toLocaleDateString("en-US",options);
-
-  res.render("list",{vaar : today_day , upcomingItem : items});
+  let today_day = date.getDate();
+  res.render("list",{listTitle : today_day , upcomingItem : items});
 });
 
 app.post("/",function(req,res)
 {
-  var item = req.body.newItem;
-  items.push(item);
+  // console.log(req.body);
+  let item = req.body.newItem;4
 
+  if(req.body.list === "Work")
+  {
+    workItems.push(item);
+    res.redirect("/work");
+  }
+  else 
+  {
+    items.push(item);
+    res.redirect("/");
+  }
+
+});
+
+app.get("/work",function(req,res)
+{
+  res.render("list",{listTitle: "Work List", upcomingItem: workItems});
+});
+
+app.post("/work",function(req,res)
+{
+  let item = req.body.newItem;
+  workItems.push(item);
   res.redirect("/");
 });
 
